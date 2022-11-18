86c0d75f0109429e317dc58e4d9cf2b957fc4ada
Jack <40455730+idogmat@users.noreply.github.com>
2022-11-18 17:53:02 +0300

    hw8

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HW6 from '../../hw06/HW6'
 import HW7 from '../../hw07/HW7'
-// import HW8 from '../../hw08/HW8'
+import HW8 from '../../hw08/HW8'
 // import HW9 from '../../hw09/HW9'
 
 function Junior() {
@@ -11,7 +11,8 @@ function Junior() {
             <hr/>
             <HW7 />
             <hr/>
-            {/*<HW8 />*/}
+            <HW8 />
+            <hr/>
             {/*<HW9 />*/}
         </div>
     )
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,32 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort':
+            let stateCopy=[...state]
+            switch (action.payload){
+                case "up":
+                   return  stateCopy.sort(compare)
+                case "down":
+                    return stateCopy.sort(compare).reverse()
+            }
 
-            return state // need to fix
-        }
-        case 'check': {
+        return stateCopy
+        case 'check':
+            let stateCopy1=[...state]
+            return stateCopy1.filter(u=>u.age>=18)
 
-            return state // need to fix
-        }
         default:
             return state
     }
+}
+function compare( a:UserType, b:UserType ) {
+    if ( a.name < b.name ){
+        return -1;
+    }
+    if ( a.name > b.name ){
+        return 1;
+    }
+    return 0;
 }
